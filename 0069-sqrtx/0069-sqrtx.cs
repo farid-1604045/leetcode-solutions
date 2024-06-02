d7abc44f7020b67b4fecaaf74729d4d7ea6daf5f
Md. Faridul Islam <geek1604045@gmail.com>
2024-06-02 18:36:00 +0600

    Time: 14 ms (96.17%), Space: 26.7 MB (92.34%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cs
+++ 0069-sqrtx/0069-sqrtx.cs
@@ -1,5 +1,16 @@
 public class Solution {
     public int MySqrt(int x) {
-        return (int)Math.Sqrt(x);
+        long low = 1, high = x;
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            long val = mid * mid;
+            if (val <= x) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return (int)high;
     }
 }
