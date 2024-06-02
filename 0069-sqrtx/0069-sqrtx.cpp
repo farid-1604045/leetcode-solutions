2235d491f5bd0f49c56d23237d0d1309094f9077
Md. Faridul Islam <geek1604045@gmail.com>
2024-06-02 18:35:22 +0600

    Time: 0 ms (100.00%), Space: 7.5 MB (39.39%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        long long low = 1, high = x;
+        while(low<=high)
+        {
+            long long mid = (low+high)/2;
+            long long val = mid*mid;
+            if(val<=x)
+            {
+                low = mid+1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+        }
+        return (int) high;
     }
 };
