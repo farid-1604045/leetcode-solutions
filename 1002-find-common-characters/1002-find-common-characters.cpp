e4108eaa966ad764823c11ace312f1f67ea1588e
Md. Faridul Islam <geek1604045@gmail.com>
2024-06-05 10:10:16 +0600

    Time: 20 ms (15.38%), Space: 14.2 MB (21.32%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        
+       if (words.empty()) return {};
+
+        unordered_map<char, int> commonCount;
+        for (char c : words[0]) {
+            commonCount[c]++;
+        }
+
+        for (auto i = 1; i < words.size(); ++i) {
+            unordered_map<char, int> currentCount;
+            for (char c : words[i]) {
+                currentCount[c]++;
+            }
+
+            for (auto& pair : commonCount) {
+                pair.second = std::min(pair.second, currentCount[pair.first]);
+            }
+
+            for (auto it = commonCount.begin(); it != commonCount.end();) {
+                if (currentCount.find(it->first) == currentCount.end()) {
+                    it = commonCount.erase(it);
+                } else {
+                    ++it;
+                }
+            }
+        }
+        
+        vector<string> result;
+        for (auto& pair : commonCount) {
+            for (int i = 0; i < pair.second; ++i) {
+                string s = "";
+                s+=pair.first;
+                result.push_back(s);
+            }
+        }
+
+        return result;
+    }
+};
