a3f0228adc85aa7f3605c8c3525e8b88df4d9627
Md. Faridul Islam <geek1604045@gmail.com>
2024-06-06 09:02:37 +0600

    Time: 129 ms (96.77%), Space: 52.1 MB (90.32%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public bool IsNStraightHand(int[] hand, int groupSize) {
+        if (hand.Length % groupSize != 0) return false;
+
+        Array.Sort(hand);
+
+        Dictionary<int, int> cardCount = new Dictionary<int, int>();
+        foreach (int card in hand) {
+            if (cardCount.ContainsKey(card)) {
+                cardCount[card]++;
+            } else {
+                cardCount[card] = 1;
+            }
+        }
+
+        foreach (int card in hand) {
+            if (cardCount[card] == 0) continue;
+
+            for (int i = 0; i < groupSize; i++) {
+                int currentCard = card + i;
+                if (!cardCount.ContainsKey(currentCard) || cardCount[currentCard] == 0) {
+                    return false;
+                }
+                cardCount[currentCard]--;
+            }
+        }
+
+        return true; 
+    }
+}
