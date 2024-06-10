374627f3f522b02121ea76cd570168e6e857d874
Md. Faridul Islam <geek1604045@gmail.com>
2024-06-10 16:17:07 +0600

    Time: 0 ms (100.00%), Space: 8 MB (44.25%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == INT_MIN) {
+            x = x * x;
+            n = n / 2;
+        }
+
+        // Handle negative exponent
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        
+        return myPowRecursive(x, n);
+    }
+private:
+    double myPowRecursive(double x, int n)
+    {
+        if (n == 0) return 1;
+        if (n == 1) return x;
+
+        // Recursion with divide and conquer
+        double half = myPowRecursive(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+};
