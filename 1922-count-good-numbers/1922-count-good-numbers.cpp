f28d490e7115212b943ef540cc2fc8048a3c16d3
Md. Faridul Islam <geek1604045@gmail.com>
2024-06-11 00:36:46 +0600

    Time: 3 ms (45.46%), Space: 7.3 MB (8.79%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -3,47 +3,50 @@ const int mod = 1000000007;
 
 class Solution {
 public:
-    long long power(long long x, long long n){
-        if(n == 0){
-            return 1;
-        }
-        long long ans = power(x, n/2);
-        ans *= ans;
-        ans %= mod;
-        if(n%2==1){
-            ans *= x;
-            ans %= mod;
-        }
-        return ans;
-    }
-public:
-    int countGoodNumbers(long long n) {
-        long long numberOfOddPlaces = n/2;
-        long long numberOfEvenPlaces = n/2 + n%2;
-        return (power(5, numberOfEvenPlaces) * power(4, numberOfOddPlaces))%mod;
-    }
-//     int countGoodNumbers(long long n) {
-//         long long odd = n / 2;
-//         long long even = (n - odd);
-//         long long oddPow = power(4, odd);
-//         long long evenPow = power(5, even);
-//         long long ans = (oddPow * evenPow) % mod;
+//     long long power(long long x, long long n){
+//         if(n == 0){
+//             return 1;
+//         }
+//         long long ans = power(x, n/2);
+//         ans *= ans;
+//         ans %= mod;
+//         if(n%2==1){
+//             ans *= x;
+//             ans %= mod;
+//         }
 //         return ans;
 //     }
-// private:
-//     long long power(int x, int n)
-//     {
-//         if (n == 0) return 1;
-
-//         // Recursion with divide and conquer
-//         long long half = power(x, n / 2);
-//         half = (half*half)%mod;
-//         if(n%2)
-//         {
-//             half = (half*x)%mod;
-//         }
-//         return half;
+// public:
+//     int countGoodNumbers(long long n) {
+//         long long numberOfOddPlaces = n/2;
+//         long long numberOfEvenPlaces = n/2 + n%2;
+//         return (power(5, numberOfEvenPlaces) * power(4, numberOfOddPlaces))%mod;
 //     }
+    int countGoodNumbers(long long n) {
+        long long odd = n / 2;
+        long long even = n/2 + n%2;
+        long long oddPow = power(4, odd);
+        long long evenPow = power(5, even);
+        int ans = (oddPow * evenPow) % mod;
+        return ans;
+    }
+private:
+    long long power(long long x, long long n)
+    {
+        if (n == 0) 
+        {
+            return 1;
+        }
+
+        // Recursion with divide and conquer
+        long long half = power(x, n / 2);
+        half = (half*half)%mod;
+        if(n%2==1)
+        {
+            half = (half*x)%mod;
+        }
+        return half;
+    }
 // private:
 //     long long power(long long base, long long exp) {
 //         long long result = 1;
