b4b1ac3282978b08db53d1d7052e17dfd601db0f
Md. Faridul Islam <geek1604045@gmail.com>
2024-06-17 09:02:48 +0600

    Time: 31 ms (56.52%), Space: 28.8 MB (82.61%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cs
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cs
@@ -1,18 +1,18 @@
 public class Solution {
-        public bool IsSquare(int n) {
-        double sqrtN = Math.Sqrt(n);
-        return sqrtN == (int)sqrtN;
-    }
-
     public bool JudgeSquareSum(int n) {
-        if (n == 0) return true;
-        while (n % 4 == 0) n /= 4;
-        if (n % 8 == 7) return false;
-        int sqrtN = (int)Math.Sqrt(n);
-        if (IsSquare(n)) return true;
-        for (int i = 1; i <= sqrtN; i++) {
-            int y = n - i * i;
-            if (IsSquare(y)) return true;
+        int end = (int)Math.Sqrt(n);
+        int start = 0;
+        while (start <= end) {
+            long a = (long)start * start;
+            long b = (long)end * end;
+            if (a + b == n) {
+                return true;
+            }
+            if (a + b < n) {
+                start++;
+            } else {
+                end--;
+            }
         }
         return false;
     }
