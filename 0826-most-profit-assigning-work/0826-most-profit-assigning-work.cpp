ab033727294056f3bc733371b69c662e140ab5e4
Md. Faridul Islam <geek1604045@gmail.com>
2024-06-18 10:53:05 +0600

    Time: 52 ms (54.38%), Space: 39.7 MB (89.18%) - LeetHub

---
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        int n = difficulty.size();
+        vector<pair<int, int>> jobs(n);
+        for (int i = 0; i < n; i++) {
+            jobs[i] = {difficulty[i], profit[i]};
+        }
+
+        sort(jobs.begin(), jobs.end());
+        sort(worker.begin(), worker.end());
+
+        int maxProfit = 0, jobIndex = 0, maxJobProfit = 0;
+
+        for (int ability : worker) {
+            while (jobIndex < n && ability >= jobs[jobIndex].first) {
+                maxJobProfit = max(maxJobProfit, jobs[jobIndex].second);
+                jobIndex++;
+            }
+            maxProfit += maxJobProfit;
+        }
+
+        return maxProfit;
+    }
+};
