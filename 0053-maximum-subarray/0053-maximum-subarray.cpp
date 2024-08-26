795823cca793e357ad809938343160d937e8568c
Md. Faridul Islam <geek1604045@gmail.com>
2024-08-26 11:59:27 +0600

    Time: 67 ms (91.29%), Space: 70.6 MB (19.03%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,22 +4,14 @@ public:
         long long maxi = LONG_MIN;
         long long sum = 0;
         int n = nums.size();
-        int start = 0;
-        int ansStart = -1, ansEnd = -1;
         for (int i = 0; i < n; i++) {
-
-            if (sum == 0) start = i; // starting index
-
+            
             sum += nums[i];
 
             if (sum > maxi) {
                 maxi = sum;
-
-                ansStart = start;
-                ansEnd = i;
             }
 
-            // If sum < 0: discard the sum calculated
             if (sum < 0) {
                 sum = 0;
             }
