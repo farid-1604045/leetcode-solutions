1ba0506a9cf8014ae69a4c917ed996b6498a54f6
Md. Faridul Islam <geek1604045@gmail.com>
2024-09-11 22:45:04 +0600

    Time: 4 ms (12.51%), Space: 8 MB (22.22%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int f(int row, int col, vector<vector<int>>& dp)
+    {
+        if(row == 0 && col == 0)
+        {
+            return 1;
+        }
+        
+        if(row < 0 || col < 0)
+        {
+            return 0;
+        }
+        
+        if(dp[row][col] != -1)
+        {
+            return dp[row][col];
+        }
+        
+        int up = f(row-1, col, dp);
+        int left = f(row, col-1, dp);
+        
+        return dp[row][col] = up + left;
+    }
+    
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m+1, vector<int>(n+1, -1));
+        return f(m-1, n-1, dp);
+    }
+};
