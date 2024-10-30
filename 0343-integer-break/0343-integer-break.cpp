9dfa1be5baec52a624762d9326936eab0b02088c
Md. Faridul Islam <geek1604045@gmail.com>
2024-10-30 12:24:57 +0600

    Time: 0 ms (100.00%), Space: 7.4 MB (93.36%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n == 2)
+        {
+            return 1;
+        }
+        
+        if(n == 3)
+        {
+            return 2;
+        }
+        
+        int product = 1;
+        while(n > 4)
+        {
+            product = product * 3;
+            n -= 3;
+        }
+        
+        return product * n;
+    }
+};
