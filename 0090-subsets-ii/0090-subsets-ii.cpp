8c8eadb3eca26bb04bc8510e3cb70d66b43e5998
Md. Faridul Islam <geek1604045@gmail.com>
2024-11-25 14:17:09 +0600

    Time: 0 ms (100.00%), Space: 9.1 MB (60.79%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    void uniqueSubsetsUtil(int ind, int n, vector<int>& arr, vector<int>& ans, vector<vector<int>>& res)
-    {
-        if(ind >= n)
-        {
-            res.push_back(ans);
-            return;
-        }
+    void findSubsets(int ind, int n, vector<int>& nums, vector<int>& ds, vector<vector<int>>& result) {       
+            result.push_back(ds);
 
-        ans.push_back(arr[ind]);
-        uniqueSubsetsUtil(ind + 1, n, arr, ans, res);
-        ans.pop_back();
-        uniqueSubsetsUtil(ind + 1, n, arr, ans, res);
+            for (int i = ind; i < n; i++) {
+                // Skip duplicates
+                if (i > ind && nums[i] == nums[i - 1]) continue;
+
+                ds.push_back(nums[i]); // Include nums[i] in the subset
+                findSubsets(i + 1, n, nums, ds, result); // Recursive call for the next element
+                ds.pop_back(); // Backtrack
+        }
     }
     
   vector < vector < int >> subsetsWithDup(vector < int > & nums) {
       
-     int n = nums.size();
-     sort(nums.begin(), nums.end());
-     vector<int> ans;
-     vector<vector<int>> res;
-     uniqueSubsetsUtil(0, n, nums, ans, res);
-     set<vector<int>> finalRes;
-     finalRes.insert(res.begin(), res.end());
-     res.assign(finalRes.begin(), finalRes.end());
-     return res;
+        sort(nums.begin(), nums.end()); // Sort to handle duplicates
+        vector<vector<int>> result;
+        vector<int> ds;
+        int n = nums.size();
+        findSubsets(0, n, nums, ds, result);
+
+        return result;
   }
 };
